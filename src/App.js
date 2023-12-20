9a3990b19e40aa3cc077aa9f481fb0b41b887312
codesantaisai <jathurshan@protonmail.com>
2023-12-20 13:58:31 +0530

    renderAllProd

--- src/App.js
+++ src/App.js
@@ -7,17 +7,29 @@ function App() {
   const FetchData = async () => {
     const res = await fetch("https://dummyjson.com/products");
     const data = await res.json();
-    setProducts(data);
-
+    console.log(data);
     if (data && data.products) {
-      console.log(data.products);
+      setProducts(data.products);
     }
   };
 
   useEffect(() => {
     FetchData();
   }, []);
-  return <div className="App"></div>;
-}
 
+  return (
+    <div className="App">
+      {products.length > 0 && (
+        <div className="products">
+          {products.map((prod) => {
+            return <span className="products__single" key={prod.id}>
+                     <img src={prod.thumbnail} alt= {prod.title}/>
+                     <span> {prod.title}</span>
+                   </span>;
+          })}
+        </div>
+      )}
+    </div>
+  );
+}
 export default App;
