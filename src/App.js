471c53150c2f8d2760738b86549d015007191d9f
codesantaisai <jathurshan@protonmail.com>
2023-12-20 14:40:28 +0530

    paginationImplemented

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import "./App.css";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
 
   const FetchData = async () => {
-    const res = await fetch("https://dummyjson.com/products");
+    const res = await fetch("https://dummyjson.com/products?limit=100");
     const data = await res.json();
     console.log(data);
     if (data && data.products) {
@@ -17,16 +18,33 @@ function App() {
     FetchData();
   }, []);
 
+  const  selectPageHandler = (selectedPage)=>{
+    setPage(selectedPage)
+  }
+
   return (
     <div className="App">
       {products.length > 0 && (
         <div className="products">
-          {products.map((prod) => {
-            return <span className="products__single" key={prod.id}>
-                     <img src={prod.thumbnail} alt= {prod.title}/>
-                     <span> {prod.title}</span>
-                   </span>;
+          {products.slice(page * 10 - 10, page * 10).map((prod) => {
+            return (
+              <span className="products__single" key={prod.id}>
+                <img src={prod.thumbnail} alt={prod.title} />
+                <span> {prod.title}</span>
+              </span>
+            );
           })}
+        </div>
+      )}
+      {products.length > 0 && (
+        <div className="pagination">
+          <span>⬅️</span>
+          {
+          [...Array(products.length / 10)].map((_,i)=>{
+            return <span onClick={()=>selectPageHandler(i+1)} className={page === i+1 ?"pagination__selected":""}>{i+1}</span>
+          })
+          }
+          <span>➡️</span>
         </div>
       )}
     </div>
